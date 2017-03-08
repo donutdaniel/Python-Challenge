cd7076850b5b280379c84bd107e4ccfc340ac3fd
Daniel Ho <danielho54@gmail.com>
2017-03-07 23:00:23 -0800

    p3

--- p3/p3.py
+++ p3/p3.py
@@ -11,21 +11,26 @@ if len(sys.argv) < 2:
 data = open(sys.argv[1], 'r').read().replace('\n', '')
 
 #global variable
-found = ""
+key = ""
 array = []
 
 #Function that takes in a string of size 9, and analyzes for correct format
 def bodyguard(s):
 	if s[0].islower() and s[1:4].isupper() and s[4].islower() and s[5:8].isupper() and s[8].islower():
 		array.append(s[1:8])
-		return s[1:8]
 
+#loop through each 9 letter combo, find bodyguards
 for i in range(0, len(data)-8):
-	temp = bodyguard(data[i:i+9])
-	if temp != None:
-		found = temp
+	bodyguard(data[i:i+9])
 
+#concatenate all small letters
+for word in array:
+	key += word[3]
 
-print(array)
-print("The key is %s" % found)
-print("http://www.pythonchallenge.com/pc/def/%s.html" % found)
+print("Array of possible 'bodyguard' strings: %s" % array)
+print()
+print("All small numbers stitched together: %s" % key)
+print("The key is %s" % key)
+print()
+print("http://www.pythonchallenge.com/pc/def/%s.html" % key)
+print("Real link is .php: http://www.pythonchallenge.com/pc/def/%s.php" % key)
