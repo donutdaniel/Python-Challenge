b422ad93dd5ba7455c8a844e4181491cb7e73a10
Daniel Ho <danielho54@gmail.com>
2017-03-07 20:40:56 -0500

    p3

--- p3/p3.py
+++ p3/p3.py
@@ -1,17 +1,19 @@
 import string
 import sys
 
+#error output if no data file provided
 if len(sys.argv) < 2:
 	print("Error. Please provide input file.")
 	sys.exit()
 
+#reads file containing string to be analyzed
+#converts to string called data
 ifile = sys.argv[1];
-
 with open(ifile, 'r') as myfile:
 	data = myfile.read().replace('\n', '')
 
+#remove punctuation
 key = data.translate(None, string.punctuation)
 
 print("key is %s" % key)
-
 print("http://www.pythonchallenge.com/pc/def/%s.html" % key)
