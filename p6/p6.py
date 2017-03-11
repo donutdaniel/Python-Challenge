f227b1d8178c66324fe4cffebbfb6ee5285d69f9
Daniel Ho <danielho54@gmail.com>
2017-03-10 20:18:07 -0800

    p6 complete

--- p6/p6.py
+++ p6/p6.py
@@ -1,10 +1,20 @@
 import re
+import zipfile
+
+begin = 90052
+channelzip = zipfile.ZipFile('channel.zip', 'r')
+comment = []
+
+def readComments(next):
+	com = channelzip.getinfo('%s.txt' % next).comment.decode("utf-8")
+	comment.append(com)
 
 def spider(next):
-	file = open('channel/' + next + '.txt', 'r');
+	file = open('channel/%s.txt' % next, 'r')
 	data = file.read()
 	#dispay data
 	print(data)
+	readComments(next)
 	#parse, then go onto next
 	nothing = parser(data)
 	if nothing != '':
@@ -16,4 +26,8 @@ def parser(data):
 	temp = re.search('the next nothing is \d+', data)
 	if temp != None:
 		data = temp.group(0)
-	return ''.join(re.findall('\d+', num));
+	return ''.join(re.findall('\d+', data));
+
+spider(begin)
+print(''.join(comment))
+
